d83d68ad202830a0879e2a89f45e1218cb1cb613
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-05-25 06:08:15 -0700

    new changes

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+
+print(f"Addition of {number1} and  {number2} is {sum_result}. ")
+print(f"substraction of {number1} and  {number2} is {difference_result}. ")
+print(f"multiplication of {number1} and  {number2} is {product_result}. ")
