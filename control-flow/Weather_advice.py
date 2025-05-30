2d0514be1ae707498f0c8b10c9a2d46108e0ae68
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-05-30 07:28:11 -0700

    weather_condition

---
+++ control-flow/Weather_advice.py
@@ -0,0 +1,16 @@
+
+User_input = input("What's the weather like today? (sunny/rainy/cold): ")
+
+if User_input == "Sunny":
+ print("Wear a t-shirt and sunglasses.")
+ 
+elif User_input == "rainy":
+ print("Don't forget your umbrella and a raincoat.")
+ 
+elif User_input == "cold":
+ print("Make  sure to wear a warm coat and a scraf")
+ 
+else:
+ print("Sorry, I don't have recommendations for this weather")
+
+
