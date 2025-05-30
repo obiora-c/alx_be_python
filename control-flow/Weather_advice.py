d3377e41b3cd349b3269a5a4bcc58186aaca648e
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-05-30 07:58:24 -0700

    Weather

--- control-flow/Weather_advice.py
+++ control-flow/Weather_advice.py
@@ -8,9 +8,7 @@ elif Weather_input == "rainy":
  print("Don't forget your umbrella and a raincoat.")
  
 elif Weather_input == "cold":
- print("Make  sure to wear a warm coat and a scraf")
+ print("Make  sure to wear a warm coat and a scraf.")
  
 else:
- print("Sorry, I don't have recommendations for this weather")
-
-
+ print("Sorry, I don't have recommendations for this weather.")
