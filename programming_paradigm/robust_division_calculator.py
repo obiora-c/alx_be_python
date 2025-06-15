6e6ebe2423dc652e14bfe2ed361b00f383474c7c
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-06-15 07:24:31 -0700

    robust_division

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,17 @@
+
+
+
+# robust_division_calculator.py
+
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+
+        result = num / den
+        return f"Result: {result}"
+    except ZeroDivisionError:
+            return "Error: Cannot divide by zero."
+
+    except ValueError:
+        return "Error: Please enter numeric values only."
