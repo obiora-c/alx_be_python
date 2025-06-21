c1fcff44f1b7af3f2b374df0f03e35e2153c020b
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-06-21 06:54:29 -0700

    Inheritance

---
+++ oop/library_system.py
@@ -0,0 +1,38 @@
+
+# library_system.py
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size  # in MB
+
+    def __str__(self):
+        return f"{super().__str__()} [EBook, {self.file_size}MB]"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"{super().__str__()} [PrintBook, {self.page_count} pages]"
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
